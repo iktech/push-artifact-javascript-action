a7fd6caedb16ce277081438e24cdf1bdc43f81e1
Igor Kolomiyets <igor.kolomiyets@iktech.io>
2021-02-24 21:44:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Push Artifact Javascript Action
+
+This action pushes the artifact version to the next stage of the flow at the https://artifactz.io.
+If version is omitted, then current version from the specified stage is getting pushed
+
+## Inputs
+### `serviceUrl`
+**Required** The URL of the artifactz.io service. 
+*Default:* https://artifactor.artifactz.io
+
+### `apiToken`
+**Required** The API token with write permissions 
+
+### `stage`
+**Required** Stage where artifact is getting pushed from
+
+### `name`
+**Required** Name of the artifact to push
+
+### `version`
+The artifact version to push
+
+## Outputs
+### `version`
+The version of the artifact that was pushed
+
+## Example
+Before adding this action to your workflow, set a secret with the API token in your project.
+Then, you can push the artifact details using this step:
+```yaml
+- name: Push Artifact
+  uses: iktech/push-artifact-javascript-action@v1.0.0
+  with:
+    apiToken: ${{ secrets.ARTIFACTZ_TOKEN }}
+    stage: Development
+    name: test
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+try {
+    const serviceUrl = core.getInput('serviceUrl');
+    const apiToken = core.getInput('apiToken');
+    const stage = core.getInput('stage');
+    const name = core.getInput('name');
+    const version = core.getInput('version');
+
+    if (!apiToken) {
+        core.setFailed('API token is required');
+    }
+
+    if (!stage) {
+        core.setFailed('Stage is required');
+    }
+
+    if (!name) {
+        core.setFailed('Artifact name is required');
+    }
+
+    let payload = {
+        stage_name: stage,
+        artifact_name: name,
+    };
+
+    if (version) {
+        payload = {
+            ...payload,
+            version,
+        }
+    }
+
+    console.log(`Publishing artifact '${name}' from the stage '${stage}'`);
+    if (version) {
+        console.log(`Pushing version ${version}`);
+    }
+
+    axios.put(`${serviceUrl}/artifacts/push`, payload, {
+        headers: {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Push Artifact Github Action v1.0.0',
+            'Authorization': `Bearer ${apiToken}`,
+        }
+    }).then(response => {
+        if (response.status !== 200) {
+            core.setFailed(`Cannot push artifact '${name}': ${response.data.message}`);
+        } else {
+            console.log(`Successfully pushed artifact '${name}' version: ${response.data.version}`);
+            core.setOutput('version', response.data.version);
+        }
+    }).catch(error => {
+        core.setFailed(error.response.data.error);
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
